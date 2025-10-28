1a6d02ee0965ab1c9ba435cf75a33a7b5c261cfc
Neil <determine2014@gmail.com>
2025-10-28 15:07:31 +0800

    Fix not found 404

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 4173,      // 固定 port
-    strictPort: true // 如果 port 被占用就報錯，不自動換
+    strictPort: true, // 如果 port 被占用就報錯，不自動換
+    historyApiFallback: true
   },
   test: {
     include: ["**/*.{test,spec}.{js,ts,jsx,tsx}"],
